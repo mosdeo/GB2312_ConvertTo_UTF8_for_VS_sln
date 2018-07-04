417a1779b6a8458a8a9790a1a4d047888426ea1d
Lin, Kao-Yuan <mosdeo@gmail.com>
2018-07-04 10:23:14 +0800

    Get full path list of *.cs file

--- main.py
+++ main.py
@@ -5,19 +5,26 @@
 # 4. 照原路徑覆蓋存檔
 
 # -*- coding: utf-8 -*-
-#範例程式 argv_samp.py
 import sys
- 
+
+def Get_List_Of_CS_file(folder):
+    from os import walk
+    # 遞迴列出所有子目錄與檔案
+    for root, dirs, files in walk(folder):
+        # print(root)
+        # print(dirs)
+        for file in files:
+            print(file[-3:])
+            print(file[-3:-1])
+            if file[-3:-1] is ".cs":
+                print(root+file)
+    print("-----------------------------------\n")
+        
 def main():
     #intValue = int(sys.argv[1])#如果要將變數搞成數字的話可以使用 int()來轉
-    print sys.argv[1]
-    print sys.argv[2]
-    print sys.argv[3]
-    print "==============="
-    print len(sys.argv)#參數一共有幾個
-    print "==============="
-    for x in sys.argv:
-        print x
- 
+    folder = sys.argv[1]
+    print("target folder: "+folder)
+    Get_List_Of_CS_file(folder)
+
 if __name__ == "__main__":
     main()
